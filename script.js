b24f2b51467e767e5299af29b7822bdec86b44a5
juanoxmar <joxmar.ramirez@gmail.com>
2020-06-07 17:31:35 -0700

    Functioning calculator!

--- script.js
+++ script.js
@@ -24,17 +24,64 @@ function operate(a , b , c) {
 //input selection and modification
 const elem = document.getElementById('inputs');
 
+//numbers go in display
 const buttonId = Array.from(document.getElementsByClassName('numBut'));
-
 const eventButton = buttonId.map(x => x.addEventListener('click', function(e){
     elem.innerHTML = elem.innerHTML + x.id
 }));
 
-//document.getElementById('9').addEventListener('click', );
-//const clickFunction = document.getElementById('9').addEventListener('click', function(e){
-//    elem.innerHTML = elem.innerHTML + document.getElementById('9').id
-//});
+let a = 0;
+let b = 0;
+let c = '';
+let d = 1
 
-//button push modifies input
-//const buttonId = Array.from(document.getElementsByClassName('but'));
-//buttonId[9].addEventListener('click', inputs(buttonId[9].id));
+//Clear Button
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', function(e){
+    elem.innerHTML = '';
+    a = 0;
+    b = 0;
+    c = '';
+    d = 1;
+});
+
+//Operation Buttons
+const addButton = document.getElementById('plus');
+const subButton = document.getElementById('subtract');
+const mulButton = document.getElementById('multiply');
+const divButton = document.getElementById('divide');
+const entButton = document.getElementById('enter');
+
+addButton.addEventListener('click', function(e){
+    a = +elem.innerHTML + a;
+    c = 'add';
+    elem.innerHTML = '';
+});
+
+subButton.addEventListener('click', function(e){
+    a = +elem.innerHTML - a;
+    c = 'sub';
+    elem.innerHTML = '';
+});
+
+mulButton.addEventListener('click', function(e){
+    a = +elem.innerHTML * d;
+    d = a;
+    c = 'mul';
+    elem.innerHTML = '';
+});
+
+divButton.addEventListener('click', function(e){
+    if ( a == 0)
+        a = +elem.innerHTML;
+    else
+        a = d / +elem.innerHTML;
+    d = a;
+    c = 'div';
+    elem.innerHTML = '';
+});
+
+entButton.addEventListener('click', function(e){
+    b = +elem.innerHTML;
+    elem.innerHTML = Math.round(operate(a , b , c) * 1000000000) / 1000000000;
+});
